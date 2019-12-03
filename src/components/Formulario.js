32148735fb5ac4e9619694ba87317b61562e26db
smunoz <sgmunoz89@gmail.com>
2019-12-02 23:22:06 -0300

    Creando Formulario

---
+++ src/components/Formulario.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Formulario() {
+
+    const handleChange = e => {
+        //cambiar el state
+    }
+    return (
+        <form>
+            <div className="input-field col s12">
+                <input 
+                    type = "text"
+                    name = "ciudad"
+                    id = "ciudad"
+                    onchange = {handleChange}
+                />
+                <label htmlFor="ciudad">Ciudad :</label>
+            </div>
+            <div className="input-field col s12">
+                <select onChange={handleChange} name="pais">
+                    <option value="">Seleccione País</option>
+                    <option value="CL">Chile</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="PE">Perú</option>
+
+                </select>
+            </div>
+            <div className="input-field col s12">
+                <input type="submit" className="waves-effect waves-light btn-large btn-block yellow accent-4" value="Obtener Clima" />
+            </div>
+        </form>
+    );
+}
+
+export default Formulario;
